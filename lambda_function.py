5f6ee11347a65e7da8d3dfd183e87c4edc609c80
Manish Bartwal <manishbartwal5080@gmail.com>
2026-01-15 16:18:27 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello Mani! This is Lambda deployed via s3 full and final CodePipeline 🚀
+        "body": "Hello Manish! This is Lambda deployed via s3 full and final CodePipeline 🚀
         This is done with the help of terraform"
     }
 
