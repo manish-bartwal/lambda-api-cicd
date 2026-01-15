51b80742293000ed2420db4a1906c096cd79a985
Manish Bartwal <manishbartwal5080@gmail.com>
2026-01-15 17:19:07 +0530

    Update response body in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello AWS lambda b"
+        "body": "Hello, my name is Manish. I had created ci/cd pipleine with the help of codepipeline."
     }
 
