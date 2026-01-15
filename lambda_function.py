d6337e14763c18c29c373bca76d85898f51dd60c
Manish Bartwal <manishbartwal5080@gmail.com>
2026-01-15 12:27:06 +0530

    Update greeting message in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello Manish! This is Lambda deployed via  CodePipeline 🚀"
+        "body": "Hello Manish! This is Lambda deployed via hello CodePipeline 🚀"
     }
 
