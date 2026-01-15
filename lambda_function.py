7c51079d7f7de6481963eb4486c313b5f4b2179c
Manish Bartwal <manishbartwal5080@gmail.com>
2026-01-15 16:59:59 +0530

    Fix body message in lambda_handler response

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello AWS lambda build number created. THis is the second build"
+        "body": "Hello AWS lambda b"
     }
 
